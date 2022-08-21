df85995d428aac01dfed5023398dcc07d946d7f5
Ramkumar.R <ramkumarrammohan@gmail.com>
2022-08-21 15:03:38 +0530

    Configured for whatsapp webhook test

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from time import sleep
-from yahoo_finance.Ticker import LiveTicker
-
-if __name__ == '__main__':
-    print('!!! app started !!!')
-
-    while (True):
-        ticker = LiveTicker()
-        ticker.add_symbol('INFY.NS')
-        ticker.add_symbol('TATASTEEL.NS')
-        err, price_list = ticker.get_current_price()
-        if err == False:
-            print(price_list)
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, Response, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+        data = "I am running"
+        return jsonify({'message': data})
+  
+  
+# function to handle /webhook 
+# methods GET & POST
+@app.route('/webhook', methods = ['GET', 'POST'])
+def webhook():
+    if(request.method == 'GET'):
+        mode =  request.args.get('hub.mode')
+        challenge =  request.args.get('hub.challenge')
+        verify_token =  request.args.get('hub.verify_token')
+        if mode == 'subscribe' and verify_token == 'ramkumar':
+            return Response(challenge, status=200)
         else:
-            print('Err: get_current_price')
-        sleep(10)
-    
-    
+            return Response(status=403)
+    elif (request.method == 'POST'):
+        print(request.json)
+        return Response(status=200)
+  
+  
+# driver function
+if __name__ == '__main__':
+    app.run(debug = False)
